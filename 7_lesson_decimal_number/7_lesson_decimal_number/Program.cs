cb9d9327d8a0c91caba4a9fe24cca08bd8cc5c84
Sergey Chemkov <schemkov@gmail.com>
2025-02-28 23:24:33 +0300

    feat: add 7_lesson_decimal_number

---
+++ 7_lesson_decimal_number/7_lesson_decimal_number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+class Program7
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        string value = Console.ReadLine().Replace('.', ',');
+        float x1 = float.Parse(value);
+        float x2 = (float)Math.Floor(x1);
+        float preresult = x1 - x2;
+        preresult *= 10;
+        int result = (int)Math.Floor(preresult);
+        Console.WriteLine(result);
+
+    }
+}
