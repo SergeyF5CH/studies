e3e92ac042d98908f1e80f114b80b71f1d84e946
Sergey Chemkov <schemkov@gmail.com>
2025-03-01 18:42:55 +0300

    fix: the solution is simple

--- 3_lesson_Chessboard/3_lesson_Chessboard/Program.cs
+++ 3_lesson_Chessboard/3_lesson_Chessboard/Program.cs
@@ -4,8 +4,8 @@
     {
         Console.WriteLine("Определение совпадения цвета клеток на шахматном поле");
 
-        int[] black = new int[] { 2, 4, 6, 8, 10, 12, 14, 16 };
-        //int[] white = new int[] { 3, 5, 7, 9, 11, 13, 15 }; - в данном случае не нужен, т.к. можно найти цвет квадрата зная только расположение кв. противоп. цветов.
+        //int[] black = new int[] { 2, 4, 6, 8, 10, 12, 14, 16 };
+        //int[] white = new int[] { 3, 5, 7, 9, 11, 13, 15 };
 
         Console.WriteLine("Введите 1-ю координату по оси Y (в пределах от 1го до 8ми: )");
         int y1 = Convert.ToInt32(Console.ReadLine());
@@ -21,34 +21,43 @@
         int modificator1 = y1 + x1;
         int modificator2 = y2 + x2;
 
-        string coincidence1 = "";
-        string coincidence2 = "";
+        //string coincidence1 = "";
+        //string coincidence2 = "";
 
-        if (Array.Exists(black, element => element == modificator1))
+        if((modificator1 % 2 ==0 && modificator2 % 2 == 0) || (modificator1 % 2 != 0 && modificator2 % 2 != 0))
         {
-            coincidence1 = "черная";
+            Console.WriteLine("Клетки одинакового цвета ");
         }
         else
         {
-            coincidence1 = "белая";
+            Console.WriteLine("Клетки разного цвета");
         }
 
-        if (Array.Exists(black, element => element != modificator2))
-        {
-            coincidence2 = "белая";
-        }
-        else
-        {
-            coincidence2 = "черная";
-        }
+        //if (Array.Exists(black, element => element == modificator1))
+        //{
+        //    coincidence1 = "черная";
+        //}
+        //else
+        //{
+        //    coincidence1 = "белая";
+        //}
 
-        if (coincidence1.Equals(coincidence2))
-        {
-            Console.WriteLine("Обе клетки одинакового цвета");
-        }
-        else
-        {
-            Console.WriteLine("Клетки не имеют одинакового цвета");
-        }
+        //if (Array.Exists(black, element => element != modificator2))
+        //{
+        //    coincidence2 = "белая";
+        //}
+        //else
+        //{
+        //    coincidence2 = "черная";
+        //}
+
+        //if (coincidence1.Equals(coincidence2))
+        //{
+        //    Console.WriteLine("Обе клетки одинакового цвета");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Клетки не имеют одинакового цвета");
+        //}
     }
 }
