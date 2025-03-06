8b4589cc58cb2391e4c2668e6923d6ea429185f6
Sergey Chemkov <schemkov@gmail.com>
2025-03-06 23:04:59 +0300

    feat: add 9_lesson_Max_Consecutive_Ones

---
+++ Max_Consecutive_Ones/Max_Consecutive_Ones/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите кол-во ячеек массива: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int[] MyArray = new int[x];
+        
+
+        for(int i = 0; i < x; i++)
+        {
+            Console.WriteLine("Введите значение ячейки массива 0 или 1: ");
+            MyArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int maxValue = 0;
+        int currentValue = 0;
+
+        Console.WriteLine("");
+        
+        foreach(int i in MyArray)
+        {
+            Console.Write($"{i}, ");
+
+            if(i == 1)
+            {
+                currentValue++;
+                if(currentValue > maxValue)
+                {
+                    maxValue = currentValue;
+                }
+                
+            }
+            else
+            {
+                currentValue = 0;
+            }
+
+        }
+
+        Console.WriteLine($"\n \nМаксимальная комбинация едениц {maxValue}");
+    }
+}
