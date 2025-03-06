50c1077180331d9c3c3f9fdbcbc13e810af3b8fa
Sergey Chemkov <schemkov@gmail.com>
2025-03-07 00:54:43 +0300

    feat: add 10_lesson_Find_Numbers_with_Even_Number_of_Digits

---
+++ 10_lesson_Find_Numbers_with_Even_Number_of_Digits/10_lesson_Find_Numbers_with_Even_Number_of_Digits/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите кол-во ячеек массива: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        string[] MyArray = new string[x];
+
+
+        for (int i = 0; i < x; i++)
+        {
+            Console.WriteLine("Введите значение ячейки массива: ");
+            MyArray[i] = Console.ReadLine();
+        }
+
+        
+        Console.WriteLine("");
+
+        List<string> even_Num = new List<string>();
+        int z = 0;
+        foreach (string i in MyArray)
+        {
+            if (MyArray[z].Length % 2 == 0) 
+            {
+                even_Num.Add(MyArray[z]);
+               
+            }
+            z++;
+
+        }
+        
+        Console.Write(even_Num.Count);
+
+
+    }
+}
+
+
