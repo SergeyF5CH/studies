ea38e81bcea3a7cb65f4f1145bf8ce56e6ea6a23
Sergey Chemkov <schemkov@gmail.com>
2025-03-09 21:25:10 +0300

    feat: add 12_lesson_Duplicate_Zeros

---
+++ 12_lesson_Duplicate_Zeros/12_lesson_Duplicate_Zeros/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = new int[8] { 1, 0, 2, 3, 0, 4, 5, 0 };
+        int _zeroCount = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                _zeroCount++;
+
+            }
+
+        }
+
+        for (int i = arr.Length - 1, j = arr.Length + _zeroCount - 1; i >= 0; i--)
+        {
+            if (j < arr.Length)
+            {
+                arr[j] = arr[i];
+            }
+            j--;
+            
+            if (arr[i] == 0)
+            {
+                if (j < arr.Length)
+                {
+                    arr[j] = 0;
+                }
+                j--;
+            }
+
+        }
+
+
+        foreach (int x in arr)
+        {
+            Console.Write($"{x} ");
+        }
+    }
+}
