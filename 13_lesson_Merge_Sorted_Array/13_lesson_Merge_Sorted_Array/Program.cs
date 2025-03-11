49f3f3f9abea3e5925fb57f6d462f45af5240b78
Sergey Chemkov <schemkov@gmail.com>
2025-03-11 21:52:35 +0300

    feat: add 13_lesson_Merge_Sorted_Array

---
+++ 13_lesson_Merge_Sorted_Array/13_lesson_Merge_Sorted_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] first_arr = new int[6] { 1, 2, 3, 0, 0, 0 };
+        int[] second_arr = new int[3] { 2, 5, 3 };
+
+        for(int i = 0 ;i < second_arr.Length; i++)
+        {
+            first_arr[first_arr.Length - 1 - i] = second_arr[second_arr.Length - 1 -i];
+        }
+
+
+        for (int i = 0; i < first_arr.Length; i++)
+        {
+            for (int j = i + 1; j < first_arr.Length; j++)
+            {
+                if (first_arr[i] > first_arr[j])
+                {
+                    int tmp = first_arr[i];
+                    first_arr[i] = first_arr[j];
+                    first_arr[j] = tmp;
+                }
+
+
+            }
+
+
+        }
+
+        foreach (int i in first_arr)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
