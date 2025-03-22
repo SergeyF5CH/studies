85118ec06bbff3b3cfa9c93a0873f98dd9490fdd
Sergey Chemkov <schemkov@gmail.com>
2025-03-22 19:55:38 +0300

    feat: add 21_lesson_Sort_Array_By_Parity

---
+++ 21_lesson_Sort_Array_By_Parity/21_lesson_Sort_Array_By_Parity/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        int[] nums = new int[x];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine(" ");
+
+        ReplaceElements(nums);
+
+        foreach (int i in nums)
+        {
+            Console.WriteLine(i + " ");
+        }
+
+
+    }
+
+    static int[] ReplaceElements(int[] nums)
+    {
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            for (int j = i; j <= nums.Length - 1; j++)
+            {
+                if (nums[i] % 2 != 0)
+                {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+
+
+                }
+            }
+
+        }
+        return nums;
+    }
+}
