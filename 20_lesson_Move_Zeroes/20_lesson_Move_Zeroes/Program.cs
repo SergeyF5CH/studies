fa5dd11590b4ca3c2f9b7e35f152fabf9eacff83
Sergey Chemkov <schemkov@gmail.com>
2025-03-22 19:55:22 +0300

    feat: add 20_lesson_Move_Zeroes

---
+++ 20_lesson_Move_Zeroes/20_lesson_Move_Zeroes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[x];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine(" ");
+
+        ReplaceElementsZero(arr);
+
+        foreach (int i in arr)
+        {
+            Console.WriteLine(i + " ");
+        }
+
+
+    }
+
+    static int[] ReplaceElementsZero(int[] arr)
+    {
+        for (int i = 0; i <= arr.Length; i++)
+        {
+            for (int j = i; j <= arr.Length - 1; j++)
+            {
+                if (arr[i] == 0)
+                {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                    
+
+                }
+            }
+        }
+        return arr;
+    }
+}
