1391fb3a0820a321e18c653ae2aea1936605bb5c
Sergey Chemkov <schemkov@gmail.com>
2025-03-22 19:55:07 +0300

    feat: add 18_lessons_Replace_elements_with_Great_Element_on_Right_Side
    need long path

---
+++ 18_lessons_Replace_elements_with_Great_Element_on_Right_Side/18_lessons_Replace_elements_with_Great_Element_on_Right_Side/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[x];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine(" ");
+
+        ReplaceElements(arr);
+
+        foreach (int i in arr)
+        {
+            Console.WriteLine(i + " ");
+        }
+
+
+    }
+
+    static int[] ReplaceElements(int[] arr)
+    {
+        int MaxR = -1;
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            int current_val = arr[i];
+            arr[i] = MaxR;
+            MaxR = Math.Max(current_val, MaxR);
+
+        }
+        return arr;
+    }
+}
