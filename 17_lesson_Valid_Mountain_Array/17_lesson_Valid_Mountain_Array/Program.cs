1238a2d1926fb25699b2fff552c4f093dfaffeab
Sergey Chemkov <schemkov@gmail.com>
2025-03-22 19:43:16 +0300

    feat: add 17_lesson_Valid_Mountain_Array

---
+++ 17_lesson_Valid_Mountain_Array/17_lesson_Valid_Mountain_Array/Program.cs
@@ -0,0 +1,58 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите кол-во чисел в массиве: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[x];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine($"Введите число {i + 1}");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        
+        bool isValidMountain = ValidMountainArray(arr);
+
+        
+        if (isValidMountain)
+        {
+            Console.WriteLine("Массив является горным.");
+        }
+        else
+        {
+            Console.WriteLine("Массив не является горным.");
+        }
+
+    }
+
+    static bool ValidMountainArray(int[] arr)
+    {
+        int n = arr.Length;
+        int x = 0;
+
+        if(n <= 3)
+        {
+            return false;
+        }
+
+        while (x < n - 1 && arr[x] < arr[x + 1])
+        {
+            x++;
+        }
+
+        if (x == 0 ||  x == n - 1)
+        {
+            return false;
+        }
+
+        while (x < n - 1 && arr[x] > arr[x + 1])
+        {
+            x++;
+        }
+        return x == n - 1; 
+
+    }
+    
+}
