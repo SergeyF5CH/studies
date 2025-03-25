195e58a33b887cd35ddde0478ab3c163419ea6ed
Sergey Chemkov <schemkov@gmail.com>
2025-03-25 21:08:26 +0300

    feat: add 25_lesson_Find_All_Numbers_Disappeared_in_an_Array

---
+++ 25_lesson_Find_All_Numbers_Disappeared_in_an_Array/25_lesson_Find_All_Numbers_Disappeared_in_an_Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+
+        IList<int> missingNums = FindDisappearedNumbers(nums);
+
+        Console.WriteLine(string.Join(", ", missingNums));
+
+
+    }
+
+    static IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        int n = nums.Length;
+        HashSet<int> numSet = new HashSet<int>(nums);
+        IList<int> result = new List<int>();
+        for(int i = 1; i <= n; i++)
+        {
+            if (!numSet.Contains(i))
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
